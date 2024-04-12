eca138a9aae0540b0e8ad10d564f176b53b6927d
Heloise <heloiseapereira@gmail.com>
2024-04-11 21:15:19 -0300

    +paciente+cadastro

--- LABMedical/src/app/app.routes.ts
+++ LABMedical/src/app/app.routes.ts
@@ -8,37 +8,41 @@ import { RegistrarConsultaComponent } from './registrar/registrar-consulta/regis
 import { RegistrarPacienteComponent } from './registrar/registrar-paciente/registrar-paciente.component';
 
 export const routes: Routes = [
-    {
-        path: "inicio",
-        component: InicioComponent
-    },
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "prontuarios",
-        component: ProntuariosComponent
-    },
-    {
-        path: "registrar",
-        children: [
-            {
-                path: "",
-                component: RegistrarComponent
-            },
-            {
-                path: "exame",
-                component: RegistrarExameComponent
-            },
-            {
-                path: "consulta",
-                component: RegistrarConsultaComponent
-            },
-            {
-                path: "paciente",
-                component: RegistrarPacienteComponent
-            }
-        ]
-    }
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'prontuarios',
+    component: ProntuariosComponent,
+  },
+  {
+    path: 'registrar',
+    children: [
+      {
+        path: '',
+        component: RegistrarComponent,
+      },
+      {
+        path: 'exame',
+        component: RegistrarExameComponent,
+      },
+      {
+        path: 'consulta',
+        component: RegistrarConsultaComponent,
+      },
+      {
+        path: 'paciente',
+        component: RegistrarPacienteComponent,
+      },
+    ],
+  },
+  // {
+  //     path: "registrar/exame",
+  //     component: RegistrarExameComponent
+  // }
 ];
