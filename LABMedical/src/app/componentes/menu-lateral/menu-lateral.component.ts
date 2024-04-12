ad3c7d88b48ad4fcb2d8af9e3228904f9c0b5a76
Heloise <heloiseapereira@gmail.com>
2024-04-12 14:54:22 -0300

    fix + roterLink

--- LABMedical/src/app/componentes/menu-lateral/menu-lateral.component.ts
+++ LABMedical/src/app/componentes/menu-lateral/menu-lateral.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.scss'
 })
