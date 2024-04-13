04da987198186c4d237cbfd95cd1728124150897
Heloise <heloiseapereira@gmail.com>
2024-04-13 20:34:50 -0300

    LOGO

---
+++ LABMedical/src/app/registrar/registrar-paciente/registrar-paciente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-registrar-paciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-paciente.component.html',
+  styleUrl: './registrar-paciente.component.scss'
+})
+export class RegistrarPacienteComponent {
+
+}
