37e479bfc9b65da4847f711e489558a06dfd9c40
Heloise <heloiseapereira@gmail.com>
2024-04-13 21:42:35 -0300

    forms com validador nativo

--- LABMedical/src/app/registrar/registrar-consulta/registrar-consulta.component.ts
+++ LABMedical/src/app/registrar/registrar-consulta/registrar-consulta.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-consulta',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registrar-consulta.component.html',
   styleUrl: './registrar-consulta.component.scss'
 })
-export class RegistrarConsultaComponent {
+export class RegistrarConsultaComponent implements OnInit {
+  registroConsultaForm!: FormGroup
+  
+  ngOnInit(){
+    this.registrarConsulta()
+  }
+  
+  registrarConsulta() {
+    this.registroConsultaForm = new FormGroup({
+      motivo: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(64)
+      ]),
+      data: new FormControl('',[
+        Validators.required
+      ]),
+      horario: new FormControl('',[
+        Validators.required
 
+      ]),
+      descricao: new FormControl('',[
+        Validators.required
+
+      ]),
+      medicacao: new FormControl(''),
+      dosagemPrecaucoes: new FormControl('',[
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(256)
+      ]),
+
+    })
+  }
 }
