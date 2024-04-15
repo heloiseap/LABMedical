fe023d6e961c77121cc6a82733e354710d795265
Heloise <heloiseapereira@gmail.com>
2024-04-15 14:59:36 -0300

    form registro exame com validadores nativos

--- LABMedical/src/app/registrar/registrar-exame/registrar-exame.component.ts
+++ LABMedical/src/app/registrar/registrar-exame/registrar-exame.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-exame',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registrar-exame.component.html',
-  styleUrl: './registrar-exame.component.scss'
+  styleUrl: './registrar-exame.component.scss',
 })
-export class RegistrarExameComponent {
+export class RegistrarExameComponent implements OnInit {
+  registroExameForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.registrarExame();
+  }
+
+  registrarExame() {
+    this.registroExameForm = new FormGroup({
+      nomeExame: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(64)
+      ]),
+      dataExame: new FormControl('',[
+        Validators.required
+      ]),
+      horarioExame: new FormControl('',[
+        Validators.required
+      ]),
+      tipoExame: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(32)
+      ]),
+      laboratorio: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(32)
+      ]),
+      urlDocumento: new FormControl('',[]),
+      resultado: new FormControl('',[
+        Validators.required
+      ]),
+    });
+  }
 }
