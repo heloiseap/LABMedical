3ff0a2e99c138ce68fea3cbeade6c804b3136eed
Heloise <heloiseapereira@gmail.com>
2024-04-21 17:20:18 -0300

    validadores e conexao com localstorage

--- LABMedical/src/app/mod-consulta/consulta.service.ts
+++ LABMedical/src/app/mod-consulta/consulta.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import consultas from '../../mock-db/consultas.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsultaService {
+  constructor() {}
 
-  constructor() { }
+  listaConsultas: Array<object> = consultas;
+
+  registrarConsulta(consultaNova: object) {
+    this.listaConsultas.push(consultaNova);
+    localStorage.setItem('consultas', JSON.stringify(this.listaConsultas));
+    console.log(this.listaConsultas)
+  }
 }
--- LABMedical/src/app/registrar/registrar-consulta/registrar-consulta.component.ts
+++ LABMedical/src/app/registrar/registrar-consulta/registrar-consulta.component.ts
@@ -1,45 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ConsultaService } from '../../mod-consulta/consulta.service';
 
 @Component({
   selector: 'app-registrar-consulta',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registrar-consulta.component.html',
-  styleUrl: './registrar-consulta.component.scss'
+  styleUrl: './registrar-consulta.component.scss',
 })
 export class RegistrarConsultaComponent implements OnInit {
-  registroConsultaForm!: FormGroup
-  
-  ngOnInit(){
-    this.registrarConsulta()
-  }
-  
-  registrarConsulta() {
+  registroConsultaForm!: FormGroup;
+  consulta = {};
+
+  constructor(private consultaService: ConsultaService) {}
+
+  ngOnInit() {
     this.registroConsultaForm = new FormGroup({
-      motivo: new FormControl('',[
+      motivo: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
-        Validators.maxLength(64)
+        Validators.maxLength(64),
       ]),
-      data: new FormControl('',[
-        Validators.required
-      ]),
-      horario: new FormControl('',[
-        Validators.required
-
-      ]),
-      descricao: new FormControl('',[
-        Validators.required
-
-      ]),
-      medicacao: new FormControl(''),
-      dosagemPrecaucoes: new FormControl('',[
+      data: new FormControl('', [Validators.required]),
+      horario: new FormControl('', [Validators.required]),
+      descricao: new FormControl('', [
         Validators.required,
         Validators.minLength(16),
-        Validators.maxLength(256)
+        Validators.maxLength(1024),
       ]),
+      medicacao: new FormControl(''),
+      dosagemPrecaucoes: new FormControl('', [
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(256),
+      ]),
+    });
+  }
 
-    })
+  registrarConsulta() {
+    if (this.registroConsultaForm.valid) {
+      this.consulta = {
+        motivo: this.registroConsultaForm.controls.motivo.value,
+        data: this.registroConsultaForm.controls.data.value,
+        horario: this.registroConsultaForm.controls.horario.value,
+        descricao: this.registroConsultaForm.controls.descricao.value,
+        medicacao: this.registroConsultaForm.controls.medicacao.value,
+        dosagemPrecaucoes:
+          this.registroConsultaForm.controls.dosagemPrecaucoes.value,
+      };
+      this.consultaService.registrarConsulta(this.consulta);
+    }
   }
 }
